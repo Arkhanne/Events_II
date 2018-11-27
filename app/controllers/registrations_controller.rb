a54213e6bd9c69d4c1615685cdd47fcce4f71958
Arkhanne <eduardbenito@icloud.com>
2018-11-27 16:24:18 +0100

    Many-to-Many Associations: Part II

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,5 @@
 class RegistrationsController < ApplicationController
+  before_action :require_signin
   before_action :set_event
 
   def index
@@ -10,10 +11,10 @@ class RegistrationsController < ApplicationController
   end
 
   def create
-    @registration = 
-      @event.registrations.new(registration_params)
+    @registration = @event.registrations.new(registration_params)
+    @registration.user = current_user
     if @registration.save
-      redirect_to event_registrations_url(@event), 
+      redirect_to event_registrations_path(@event), 
                   notice: "Thanks for registering!"
     else
       render :new
@@ -23,7 +24,7 @@ class RegistrationsController < ApplicationController
 private
 
   def registration_params
-    params.require(:registration).permit(:name, :email, :how_heard)
+    params.require(:registration).permit(:how_heard)
   end
 
   def set_event
