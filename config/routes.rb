5476d96adc232a90891f4e981edf5544423eea1c
Arkhanne <eduardbenito@icloud.com>
2018-11-28 12:17:50 +0100

    Through Associations: Part II

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :likes
   resource :session
-
+  
   get "signup" => "users#new"
   resources :users
   root "events#index"
   resources :events do
     resources :registrations
+    resources :likes
   end
 end
